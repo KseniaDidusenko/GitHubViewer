a4da0785a7f986e90bed71291f713838a40b4e62
Oksana Didusenko <ksenia.didusenko@gmail.com>
2020-06-30 17:18:59 +0300

    Initial commit.

---
+++ GitHubViewer/GitHubViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubViewer
+//
+//  Created by Ksenia on 30.06.2020.
+//  Copyright © 2020 Ksenia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
