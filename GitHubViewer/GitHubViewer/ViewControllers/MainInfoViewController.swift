ca84a3e3933b06165a20597b352efa4c05b622a9
Oksana Didusenko <ksenia.didusenko@gmail.com>
2020-07-01 23:36:16 +0300

    Added VC.

---
+++ GitHubViewer/GitHubViewer/ViewControllers/MainInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainInfoViewController.swift
+//  GitHubViewer
+//
+//  Created by Ksenia on 30.06.2020.
+//  Copyright © 2020 Ksenia. All rights reserved.
+//
+
+import UIKit
+
+class MainInfoViewController: UIViewController {
+
+  // MARK: - Public properties
+
+  // MARK: - Outlets
+
+  // MARK: - Private properties
+
+  // MARK: - View controller view's lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  // MARK: - Navigation
+
+  // MARK: - Actions
+
+  // MARK: - Public API
+
+  // MARK: - Private API
+
+}
---
+++ GitHubViewer/GitHubViewer/ViewControllers/SignInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewController.swift
+//  GitHubViewer
+//
+//  Created by Ksenia on 30.06.2020.
+//  Copyright © 2020 Ksenia. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+  // MARK: - Public properties
+
+  // MARK: - Outlets
+
+  // MARK: - Private properties
+
+  // MARK: - View controller view's lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .purple
+  }
+
+  // MARK: - Navigation
+
+  // MARK: - Actions
+
+  // MARK: - Public API
+
+  // MARK: - Private API
+
+}
